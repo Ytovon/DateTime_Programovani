9fcc746e5fb7e21c86d8feb8e0b55ff6c3e175e8
Ytovon <46honza2007@gmail.com>
2025-01-10 12:41:33 +0100

    Přidejte soubory projektu.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using ConsoleApp1;
+using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Continue = true;
+
+            while (Continue)
+            {
+                int input = TimeHelper.GetIntInRange("1 - Získej dnešní den v týdnu, 2 - Získej momentální čas, 3 - pro posun podle vámi stanoveneho počtu dní, 4 - pro rozdil mezi vámi stanoveným časem a momentálním časem, 5 - odejít");
+
+                switch (input)
+                {
+                    case 1:
+                        Console.WriteLine(TimeHelper.GetDayOfWeek());
+                        break;
+                    case 2:
+                        Console.WriteLine(TimeHelper.GetCurrentTime());
+                        break;
+                    case 3:
+                        Console.WriteLine(TimeHelper.CalculateAddDays().ToString("D"));
+                        break;
+                    case 4:
+                        Console.WriteLine(TimeHelper.CalculateDifferenceNow());
+                        break;
+                    case 5:
+                        Continue = false;
+                        break;
+                    default:
+                        Console.WriteLine("Špatně zadaný vstup...");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/TimeHelper.cs
@@ -0,0 +1,75 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class TimeHelper
+    {
+        static string format = "dd/MM/yyyy";
+
+        public static string GetDayOfWeek()
+        {
+            CultureInfo currentCulture = CultureInfo.CurrentCulture;
+            DateTime dateTime = DateTime.Now;
+
+            return dateTime.ToString("dddd", currentCulture);
+        }
+
+        public static string CalculateDifferenceNow()
+        {
+            DateTime dateTime = GetUserDate("Napište datum ve formátu dd/MM/yyyy (např. 10/12/2024)");
+            DateTime now = DateTime.Now;
+
+            TimeSpan difference = dateTime - now;
+
+            return difference.Days.ToString() + " den/dní";
+        }
+
+        public static DateTime GetUserDate(string message)
+        {
+            DateTime parsedDate;
+
+            Console.WriteLine(message);
+            while (!DateTime.TryParseExact(Console.ReadLine(), format, CultureInfo.CurrentCulture, DateTimeStyles.None, out parsedDate))
+            {
+                Console.WriteLine("Špatně napsaný formát. Zkuste to znovu!");
+            };
+
+            return parsedDate;
+        }
+
+        public static int GetIntInRange(string message)
+        {
+            int value;
+
+            Console.WriteLine(message);
+            while (!int.TryParse(Console.ReadLine(), out value))
+            {
+                Console.WriteLine("Zadejte platné číslo.");
+            }
+
+            return value;
+        }
+
+        public static DateTime CalculateAddDays()
+        {
+            int daysToAdd = GetIntInRange("Napiš o kolik dní chcete čas posunout (+/-)");
+            DateTime dateTime = GetUserDate("Napište datum ve formátu dd/MM/yyyy (např. 10/12/2024)");
+
+            return dateTime.AddDays(daysToAdd);
+        }
+
+        public static string GetCurrentTime()
+        {
+            CultureInfo currentCulture = CultureInfo.CurrentCulture;
+            DateTime dateTime = DateTime.Now;
+
+            return dateTime.ToString("HH:mm:ss", currentCulture);
+        }
+    }
+}
